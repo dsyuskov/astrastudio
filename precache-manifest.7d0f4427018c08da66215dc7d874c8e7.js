10d848d86d59f8dcd1cadb731c3c55115711c863
int <yuskovss@gmail.com>
2020-02-09 23:08:23 +0400

    Updates

---
+++ precache-manifest.7d0f4427018c08da66215dc7d874c8e7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e199c70448f18ea19ad688527852e4f",
+    "url": "/astrastudio/index.html"
+  },
+  {
+    "revision": "77b00b93b8fe35c65245",
+    "url": "/astrastudio/static/css/main.0c50c52b.chunk.css"
+  },
+  {
+    "revision": "d8840d0d14dda4d7c458",
+    "url": "/astrastudio/static/js/2.1d366fd9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/astrastudio/static/js/2.1d366fd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77b00b93b8fe35c65245",
+    "url": "/astrastudio/static/js/main.2dbabae0.chunk.js"
+  },
+  {
+    "revision": "9ec8fc5d4ca3ff2df5cb",
+    "url": "/astrastudio/static/js/runtime-main.09b277dd.js"
+  },
+  {
+    "revision": "972e8bca00507601c1aba3b50f668eb7",
+    "url": "/astrastudio/static/media/background-screen.972e8bca.png"
+  },
+  {
+    "revision": "3a11c8fa2be538fba390e3a894c952ba",
+    "url": "/astrastudio/static/media/background.3a11c8fa.png"
+  },
+  {
+    "revision": "7bcf559d961276cc05aef857f9856ca6",
+    "url": "/astrastudio/static/media/header.7bcf559d.png"
+  },
+  {
+    "revision": "324c7fbc8517cacee7f1f4bcbdfae262",
+    "url": "/astrastudio/static/media/side-bg.324c7fbc.jpg"
+  },
+  {
+    "revision": "159c96d77084cc6ce6a62929b3596c79",
+    "url": "/astrastudio/static/media/task-kings.159c96d7.png"
+  },
+  {
+    "revision": "94ef6c8d31b03b1ad7e56f54cb9dc246",
+    "url": "/astrastudio/static/media/task-time.94ef6c8d.png"
+  },
+  {
+    "revision": "935c7e99ed4f2d613c6fef68efcf0fc0",
+    "url": "/astrastudio/static/media/task-tournaments.935c7e99.png"
+  }
+]);
